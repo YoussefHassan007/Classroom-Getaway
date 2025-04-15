a4d9a9fc6a3daa6213cbff25a2be08422538c948
YoussefHassan007 <YHwork007@gmail.com>
2025-04-15 16:11:48 +0400

    Add files via upload

---
+++ classroom-getaway/Enemy/BaseEnemy.gd
@@ -0,0 +1,10 @@
+extends Resource
+@export  var name: String = "Enemy"
+@export var texture: Texture = null
+@export var health : int = 50
+@export var damage : int = 20
+@export var exp_given: int = 35
+@export var defense: int = 10
+@export var speed: int = 20
+@export var accuracy: float = 0.9
+@export var crit_chance: float = 0.2
---
+++ classroom-getaway/Player/State.gd
@@ -0,0 +1,7 @@
+extends Node
+var current_health = 35 
+var max_health = 35
+var damage = 20
+var defense = 1
+var speed = 17
+var base_crit = 0.8
